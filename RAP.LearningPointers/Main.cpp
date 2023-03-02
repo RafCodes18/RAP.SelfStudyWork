3dd5b6f4e430e9512ef4a1254e2d0f2adef7faac
Rafael Parra <700175134@fvtc.edu>
2023-03-02 15:27:58 -0600

    Added new project and moved over pointer code to it. For each new learning objective, create a new project with a new Main.cpp to practice code in. Central learning repo for C++

---
+++ RAP.LearningPointers/Main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>;
+#include <conio.h>;
+#include <string>;
+using namespace std;
+
+
+struct Person {
+	string name;
+	int age;
+};
+
+
+
+void PrintPerson(Person* p) //accept the address of Person struct passed in
+{
+	//because p is a pointer, we use the arrow to dereference and access its memebers	
+	cout << p->name << " is " << p->age << " years old.";
+}
+
+
+
+int main()
+{
+	//p NOT A POINTER
+	Person p;  //on the stack 
+	p.age = 12;
+	p.name = "Jerry";
+
+	PrintPerson(&p); //pass in the address of p
+	cout << "\n";
+
+
+
+	//p2 POINTER		   	//pointer named p2 that points to a Person struct on the heap
+	Person* p2 = new Person; //on the heap
+	p2->name = "Carl";
+	p2->age = 29;
+
+	//since it is already an adress, do not need &
+	PrintPerson(p2);
+
+
+	_getch();
+	return 0;
+}
--- RAP.SelfStudyWork/Main.cpp
+++ RAP.SelfStudyWork/Main.cpp
@@ -1,45 +1,17 @@
-#include <iostream>;
-#include <conio.h>;
-#include <string>;
-using namespace std;
 
 
-struct Person {
-	string name;
-	int age;
-};
+/*
 
+	Rafael Parra
+	3/2/2023
 
+	Learning C++
 
-void PrintPerson(Person *p) //accept the address of Person struct passed in
-{
-		//because p is a pointer, we use the arrow to dereference and access its memebers	
-	cout << p->name << " is " << p->age << " years old.";
-}
+*/
 
 
 
-int main()
-{
-	//p NOT A POINTER
-	Person p;  //on the stack 
-	p.age = 12;
-	p.name = "Jerry";
-
-	PrintPerson(&p); //pass in the address of p
-					cout << "\n";
-
-
-
-	//p2 POINTER		   	//pointer named p2 that points to a Person struct on the heap
-	Person *p2 = new Person; //on the heap
-	p2->name = "Carl";
-	p2->age = 29;
-
-	//since it is already an adress, do not need &
-	PrintPerson(p2);
-	
-
-	_getch();
-	return 0;
-}
+/*This solution is for learning, testing, and writing my own code.
+  Each project conains a main.cpp file to work with.
+  Each project should be around the topic of the week, this weeks(3/2/23) being Arrays, last week pointers, next week File I/O, etc.
+*/
