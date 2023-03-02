69af324860d5871c689c41bec1e73826ecaee32f
Rafael Parra <700175134@fvtc.edu>
2023-03-02 17:10:46 -0600

    studied dynamic memory with arrays. Added  MoreArrays() function

--- LearningArrays/Main.cpp
+++ LearningArrays/Main.cpp
@@ -3,6 +3,67 @@
 #include<string>;
 using namespace std;
 
+//arrays and dynamic memory storage of values
+void ArrayOnHeap()
+{
+
+	//USING CONST
+		//if array is on the stack, we need to know before hand what the size will be, so NUM_PEOPLE has to be const
+	const int NUM_PEOPLE = 4;
+	int ages1[NUM_PEOPLE] = { 12, 55, 32, 31 };  //if no const above, this would error, must know size when on stack, or use pointer
+
+	//USING POINTER
+	int num_people = 0;					//ALLOWS DYNAMIC ARRAY CREATION
+	cout << "How many ages would you like to track?: "; cin >> num_people; //ask user for size of array
+
+			//new indicates we are now on the heap, pointer named ages. Anytime we use 'new', we are returned a pointer(address not value) to the first element
+	int *ages = new int[num_people];
+
+	for (int i = 0; i < num_people; i++)
+	{
+		std::cout << "Age of person " << (i + 1) << ": ";
+		std::cin >> ages[i];
+	}
+
+	for (int i = 0; i < num_people; i++)
+	{
+		std::cout << ages[i] << "\n";
+	}
+}
+
+
+struct Person {
+	string name; 
+	int age;
+};
+
+//structuring data with arrays
+void MoreArrays()
+{
+	const int NUM_PEOPLE = 5;
+
+
+	//array of integers named ages that holds 5 elements, then sets the values of each 
+	int ages[NUM_PEOPLE] = { 12, 22, 56, 33, 15 };
+
+	//array of strings called 'names' that holds 5 elements, then sets the values of each 
+	string names[NUM_PEOPLE] = { "Harry", "Suzzy", "Shelly", "Sandy", "Carly" };
+
+
+
+	//an array Named 'people', that holds 5 Person structs
+	Person people[5];
+
+	for (int i = 0; i < NUM_PEOPLE; i++)  //loop through the array of people objects, and change their .age to ages[i]
+	{
+		people[i].age = ages[i];
+		people[i].name = names[i];
+		cout << people[i].name << " is " << people[i].age << " years old! \n";
+	}
+	
+}
+
+//should print an address then a value. names of arrays are pointers to the first element of that array
 void LearnArrays()
 {
 
@@ -29,7 +90,7 @@ void LearnArrays()
 
 /*linebreak*/cout << "\n";
 
-	cout << *ages; // this dereferences the address of the first element, printing the values
+	cout << *ages<<"\n"; // this dereferences the address of the first element, printing the values
 
 }
 
@@ -37,7 +98,12 @@ void LearnArrays()
 //first code to run, the entry point of the program
 int main()
 {
+	//fundamentals
 	LearnArrays();
+	MoreArrays();
+
+	//arrays and memory
+	ArrayOnHeap();
 
 	_getch();
 	return 0;
